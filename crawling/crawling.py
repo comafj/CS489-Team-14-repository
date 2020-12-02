237b8cf86fa6ef4088687307d87f07a81fa34613
Sangwoo Jung <48044261+SangwooJung98@users.noreply.github.com>
2020-12-02 15:23:17 +0900

    Added more naver news links

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -38,6 +38,16 @@ import os
 # url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=014&aid=0004535517'
 # url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=023&aid=0003578904'
 
+### 뉴스 기사 추가 ###
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=003&aid=0010218099'
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=001&aid=0012053230'
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=057&aid=0001524572'
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=001&aid=0012053948'
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=015&aid=0004460126'
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&oid=047&aid=0002293990'
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&oid=018&aid=0004798939'
+# url = 'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&oid=018&aid=0004798885'
+
 print("###################################################")
 print("### Do not touch chrome webpage during crawling ###")
 print("###################################################")
